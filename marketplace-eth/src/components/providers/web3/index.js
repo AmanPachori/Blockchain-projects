efdde8753770346de67e7e95942f93a4d7b13192
AmanPachori <amangirrajpachori2002@gmail.com>
2023-02-11 01:27:12 +0530

    minor fixes

--- marketplace-eth/src/components/providers/web3/index.js
+++ marketplace-eth/src/components/providers/web3/index.js
@@ -1,11 +1,27 @@
-const { createContext, useContext } = require("react");
-
+const { createContext, useContext, useEffect } = require("react");
+import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
 
 const Web3Context = createContext(null);
 
 export default function Web3Provider({children}){
+
+  useEffect(()=>{
+    const loadProvider = async () =>{
+
+      const provider = await detectEthereumProvider();
+      if(provider){
+
+      }
+      else{
+        console.error("Please install metamsk")
+      }
+    }
+
+    loadProvider();
+  })
   return(
-    <Web3Context.Provider value={{test:"Hello"}}>
+    <Web3Context.Provider value={{}}>
       {children}
     </Web3Context.Provider>
   )
